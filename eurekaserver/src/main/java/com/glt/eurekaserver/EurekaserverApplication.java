8f34ed3f03ae7bdeb469c320a35e398c5c68670d
geliangtao <emailglt@163.com>
2019-11-10 21:01:56 +0800

    测试 idea提交代码

--- eurekaserver/src/main/java/com/glt/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/glt/eurekaserver/EurekaserverApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaserverApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaserverApplication.class, args);
+        SpringApplication.run(EurekaserverApplication.class,args);
     }
 
 }
